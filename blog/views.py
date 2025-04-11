22c1e16b74c19b9205454e604b895b92c28a9310
adilX19 <adil@matos.ai>
2025-04-11 15:49:49 +0500

    Blog Creations feature added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Article
 
-# Create your views here.
+
+def create_article(request):
+
+	if request.method == 'POST':
+		title = request.POST.get('title', None)
+		content = request.POST.get('content', None)
+
+
+		Article.objects.create(
+			author=request.user,
+			title=title,
+			content=content
+		)
+
+		return render(request, 'blog/list.html', {
+			"articles": request.user.articles.all()
+		})
+
+	elif request.method == 'GET':
+		return render(request, 'blog/form.html', {})
